b6f0ef3d2273e5bfe461cdb0e2acee521c40f0ee
jalil wahdatehagh <j.wahdatehagh@gmail.com>
2015-01-23 10:17:56 +0100

    documentation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-// helpers
+/**
+* Helper object holding handy functions,
+* some game logic and repeating numbers
+**/
 var helpers = {};
 helpers.xCanvas = 505;
 helpers.yCanvas = helpers.yTile * helpers.rows;
@@ -6,28 +9,53 @@ helpers.columns = 5;
 helpers.rows = 6;
 helpers.xTile = 101;
 helpers.yTile = 83;
-helpers.xTileCenter = helpers.xTile/2;
-helpers.yTileCenter = helpers.yTile/2;
 helpers.charHeight = 102;
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
+ * @param {number} min
+ * @param {number} max
+ * @return {number} random integer between min and max
  */
 helpers.getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+/**
+ * Returns a the x position of an entity based on the column
+ * @param {number} column
+ * @return {number} x position
+ */
 helpers.columnsToXPosition = function(column) {
     return column * this.xTile - this.xTile;
 };
+
+/**
+ * Returns a the y position of an entity based on the row
+ * @param {number} row
+ * @return {number} y position
+ */
 helpers.rowsToYPosition = function(row) {
     return row * this.yTile - this.charHeight;
 };
+
+/**
+ * Game logic to check whether a star has been collected
+ * @param {Player} player
+ * @param {Star} star
+ */
 helpers.checkIfStarCollected = function(player, star) {
     if (player.column === star.column && player.row === star.row) {
         player.collectStar();
         star.newPosition();
     }
 };
+
+/**
+ * Game logic to check whether the player collides with one of the enemies
+ * @param {Player} player
+ * @param {Array} array holding all the enemies
+ */
 helpers.checkForCollision = function(player, allEnemies) {
     allEnemies.forEach(function(enemy) {
         if (enemy.column === player.column && enemy.row === player.row) {
@@ -38,10 +66,11 @@ helpers.checkForCollision = function(player, allEnemies) {
 
 
 /**
-Parent Class for all Game Entities
-@param {string} link to sprite
-@param {number} column position
-@param {number} row position
+* Parent Class for all Game Entities
+* @param {string} link to sprite
+* @param {number} column position
+* @param {number} row position
+* @constructor
 **/
 var Entity = function(sprite, column, row) {
     console.info('creating an Entity');
@@ -52,8 +81,10 @@ var Entity = function(sprite, column, row) {
     this.y = helpers.rowsToYPosition(this.row);
 };
 
-
-// Draw the entity on the screen, required method for game
+/**
+ * Draw the entity on the screen
+ * @param {boolean} render by x/y position instead of column/row
+ */
 Entity.prototype.render = function(byXY) {
     var x, y;
     if (byXY) {
@@ -67,7 +98,11 @@ Entity.prototype.render = function(byXY) {
 };
 
 
-// Enemies our player must avoid
+/**
+* Enemies our player must avoid
+* @param {number} speed of the enemy
+* @constructor
+**/
 var Enemy = function(speed) {
     this.speed = speed;
     this.spriteForward = 'images/enemy-bug.png';
@@ -81,17 +116,18 @@ var Enemy = function(speed) {
 Enemy.prototype = Object.create(Entity.prototype);
 Enemy.prototype.constructor = Enemy;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+ * Update the enemy's position
+ * @param {number} a time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply movement by the dt parameter to ensure the game
+    // runs at the same speed for all computers
     this.x += this.speed * dt;
 
-    // recompute column
+    // recompute column (the enemy is rendered by x/y position 
+    // but for collision calculation we use column/row)
     this.column = Math.round(this.x / helpers.xTile) + 1;
-
 
     // if enemy moves off the canvas, let it turn around
     var offCanvasRight = this.x > helpers.xCanvas;
@@ -102,20 +138,33 @@ Enemy.prototype.update = function(dt) {
     }
 
 };
+
+/**
+ * Update the enemy's row position
+ */
 Enemy.prototype.randomizeRow = function() {
     var row = helpers.getRandomInt(2,5);
     this.row = row;
+
+    // recompute y position (the enemy is rendered by x/y position 
+    // but for collision calculation we use column/row)
     this.y = helpers.rowsToYPosition(row);
 };
+
+/**
+ * Change the enemy's direction
+ */
 Enemy.prototype.changeDirection = function() {
     this.speed = -this.speed;
     this.sprite = this.forward ? this.spriteReverse : this.spriteForward;
     this.forward = ! this.forward;
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+/**
+* Player class - our lovely player
+* @constructor
+**/
 var Player = function() {
     this.newPosition();
     this.starCount = 0;
@@ -124,25 +173,44 @@ var Player = function() {
 Player.prototype = Object.create(Entity.prototype);
 Player.prototype.constructor = Player;
 
+/**
+ * Call this when a player collects a star.
+ */
 Player.prototype.collectStar = function() {
     this.starCount += 1;
     console.info('Yay! Your score is ' + this.starCount);
 };
+
+/**
+ * Update the player's position
+ */
 Player.prototype.newPosition = function() {
     this.column = helpers.getRandomInt(1,5);
     this.row = 6;
 };
+
+/**
+ * Reset the player's position
+ */
 Player.prototype.reset = function() {
     this.starCount = 0;
     this.newPosition();
     console.info('You lost... Score is back to 0');
 };
+
+/**
+ * Show/Update the player's score
+ */
 Player.prototype.showScore = function() {
     ctx.fillStyle="yellow";
     ctx.font = "30px Serif";
     ctx.fillText("Score: " + this.starCount, 10, 100);
-}
+};
 
+/**
+ * Handle keyboard input events and move the player
+ * @param {string} the key we're releasing
+ */
 Player.prototype.handleInput = function(key) {
     if (key == 'right' && this.column < helpers.columns) {
         return this.column += 1;
@@ -158,6 +226,11 @@ Player.prototype.handleInput = function(key) {
     }
 };
 
+
+/**
+* Star class - our player loves to collect it
+* @constructor
+**/
 var Star = function() {
     var column = helpers.getRandomInt(1,5);
     var row = helpers.getRandomInt(2,5);
@@ -166,22 +239,26 @@ var Star = function() {
 Star.prototype = Object.create(Entity.prototype);
 Star.prototype.constructor = Star;
 
+/**
+ * Update the star's position
+ */
 Star.prototype.newPosition = function() {
     this.column = helpers.getRandomInt(1,5);
     this.row = helpers.getRandomInt(2,5);
 };
 
-// Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [new Enemy(120), new Enemy(200), new Enemy(220), new Enemy(60), new Enemy(170)];
-// var allEnemies = [new Enemy(50)];
+
+// Place a player instance in a variable called player
 var player = new Player();
+
+// Place a star instance in a variable called star
 var star = new Star();
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and send the keys to Player.handleInput()
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
